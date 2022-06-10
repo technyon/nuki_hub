bee2b1f191e3a2d2a9dca9605f5dece83a8bbc43
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2022-06-10 08:29:19 +0000

    Add MQTT SSL preference keys

--- WebCfgServer.cpp
+++ WebCfgServer.cpp
@@ -168,6 +168,21 @@ bool WebCfgServer::processArgs(String& message)
             _preferences->putString(preference_mqtt_opener_path, value);
             configChanged = true;
         }
+        else if(key == "MQTTCA")
+        {
+            _preferences->putString(preference_mqtt_ca, value);
+            configChanged = true;
+        }
+        else if(key == "MQTTCRT")
+        {
+            _preferences->putString(preference_mqtt_crt, value);
+            configChanged = true;
+        }
+        else if(key == "MQTTKEY")
+        {
+            _preferences->putString(preference_mqtt_key, value);
+            configChanged = true;
+        }
         else if(key == "HOSTNAME")
         {
             _preferences->putString(preference_hostname, value);
@@ -437,6 +452,9 @@ void WebCfgServer::buildMqttConfigHtml(String &response)
     printInputField(response, "MQTTPORT", "MQTT Broker port", _preferences->getInt(preference_mqtt_broker_port), 5);
     printInputField(response, "MQTTUSER", "MQTT User (# to clear)", _preferences->getString(preference_mqtt_user).c_str(), 30);
     printInputField(response, "MQTTPASS", "MQTT Password", "*", 30, true);
+    printInputField(response, "MQTTCA", "MQTT SSL CA Certificate", _preferences->getString(preference_mqtt_ca).c_str(), 180);
+    printInputField(response, "MQTTCRT", "MQTT SSL Client Certificate", _preferences->getString(preference_mqtt_crt).c_str(), 180);
+    printInputField(response, "MQTTKEY", "MQTT SSL Client Key", _preferences->getString(preference_mqtt_key).c_str(), 180);
     printInputField(response, "NETTIMEOUT", "Network Timeout until restart (seconds; -1 to disable)", _preferences->getInt(preference_network_timeout), 5);
     response.concat("</table>");
     response.concat("<br><INPUT TYPE=SUBMIT NAME=\"submit\" VALUE=\"Save\">");
