9630d64c4a51611b56c02150a7e16e380e84e9ef
iranl <iranl@users.noreply.github.com>
2025-03-05 17:09:54 +0100

    Set next version to 9.10

--- src/Config.h
+++ src/Config.h
@@ -2,8 +2,8 @@
 
 #include "sdkconfig.h"
 
-#define NUKI_HUB_VERSION "9.09"
-#define NUKI_HUB_VERSION_INT (uint32_t)909
+#define NUKI_HUB_VERSION "9.10"
+#define NUKI_HUB_VERSION_INT (uint32_t)910
 #define NUKI_HUB_BUILD "unknownbuildnr"
 #define NUKI_HUB_DATE "2025-03-03"
 
