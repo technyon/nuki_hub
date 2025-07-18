17eee0ad7d840b40d9b2e8f87b08af3af48ae8ee
iranl <iranl@users.noreply.github.com>
2025-07-18 22:26:38 +0200

    Revert custom hosted and wifi_remote

--- resources/espressif__esp_wifi_remote/scripts/copyright_header.h
+++
@@ -1,6 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-// This file is auto-generated
--- resources/espressif__esp_wifi_remote/test/smoke_test/components/esp_hosted/include/esp_hosted_wifi_api.h
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Unlicense OR CC0-1.0
- */
-#pragma once
-#include "esp_wifi.h"
-#include "esp_hosted_api.h"
