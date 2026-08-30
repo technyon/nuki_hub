4c8b6f502f5962193d7eecb7dde81960ea6ce05e
technyon <14868793+technyon@users.noreply.github.com>
2026-08-30 08:24:18 +0000

    Set next release version

--- src/Config.h
+++ src/Config.h
@@ -2,8 +2,8 @@
 
 #include "sdkconfig.h"
 
-#define NUKI_HUB_VERSION "9.15"
-#define NUKI_HUB_VERSION_INT (uint32_t)915
+#define NUKI_HUB_VERSION "9.16"
+#define NUKI_HUB_VERSION_INT (uint32_t)916
 #define NUKI_HUB_BUILD "unknownbuildnr"
 #define NUKI_HUB_DATE "2026-08-30"
 #define NUKI_HUB_DATE "2026-08-30"
